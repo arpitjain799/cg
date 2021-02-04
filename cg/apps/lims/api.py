d061099acf8cb5f3a5ae56fa02f96f35cb01fadf
mayabrandi <maya.brandi@scilifelab.se>
2021-02-04 08:06:00 +0100

    notes

--- cg/apps/lims/api.py
+++ cg/apps/lims/api.py
@@ -14,6 +14,7 @@ from cg.exc import LimsDataError
 
 from cg.constants.lims import MASTER_STEPS_UDFS, PROP2UDF
 from .order import OrderHandler
+from typing import Set, List, Optional, Dict
 
 requests_cache.install_cache(backend="memory")
 
@@ -44,56 +45,148 @@ class LimsAPI(Lims, OrderHandler):
         lconf = config["lims"]
         super(LimsAPI, self).__init__(lconf["host"], lconf["username"], lconf["password"])
 
-    def sample(self, lims_id: str):
-        """Fetch a sample from the LIMS database."""
+    # Not Used!? Can be deleted!!!!?
+    def _export_artifact(lims_artifact) -> Dict[str, str]:
+        """Get data from a LIMS artifact."""
+        return {"id": lims_artifact.id, "name": lims_artifact.name}
+
+    def process(self, process_id: str) -> Process:  # !!!!!!!! # Not used!? Can be deleted!!!!?
+        """Get LIMS process."""
+        return Process(self, id=process_id)
+
+    def process_samples(lims_process: Process) -> Generator[str, None, None]:
+        """Retrieve LIMS input samples from a process."""
+        for lims_artifact in lims_process.all_inputs():
+            for lims_sample in lims_artifact.samples:
+                yield lims_sample.id
+
+    def update_project(self, lims_id: str, name=None) -> None:
+        """Update information about a project."""
+        lims_project = Project(self, id=lims_id)
+        if name:
+            lims_project.name = name
+            lims_project.put()
+
+    ## help functions part of cg_lims simply
+    def _sort_by_date_run(sort_list: list):
+        """
+        Sort list of tuples by parent process attribute date_run in descending order.
+
+        Parameters:
+            sort_list (list): a list of tuples in the format (date_run, elem1, elem2, ...)
+
+        Returns:
+            sorted list of tuples
+        """
+        return sorted(sort_list, key=lambda sort_tuple: sort_tuple[0], reverse=True)
+
+    def _most_recent_date(self, dates: list) -> Optional[str]:
+        """
+        Gets the most recent date from a list of dates sorted by date_run
+
+        Parameters:
+            dates (list): a list of tuples in the format (date_run, date), sorted by date_run
+            descending
+
+        Returns:
+            The date in the first tuple in dates
+        """
+        sorted_dates = self._sort_by_date_run(dates)
+        date_run_index = 0
+        date_index = 1
+
+        return sorted_dates[date_run_index][date_index] if dates else None
+
+    ### PUT ROUTS
+    def update_sample(self, lims_id: str, sex=None, target_reads: int = None, name: str = None, **kwargs) -> None:
+        """Update information about a sample."""
         lims_sample = Sample(self, id=lims_id)
-        data = self._export_sample(lims_sample)
-        return data
 
-    def samples_in_pools(self, pool_name, projectname):
-        """Fetch all samples from a pool"""
-        return self.get_samples(udf={"pool name": str(pool_name)}, projectname=projectname)
+        if sex:
+            lims_gender = REV_SEX_MAP.get(sex)
+            if lims_gender:
+                lims_sample.udf[PROP2UDF["sex"]] = lims_gender
+        if name:
+            lims_sample.name = name
+        if isinstance(target_reads, int):
+            lims_sample.udf[PROP2UDF["target_reads"]] = target_reads
 
-    @staticmethod
+        for key, value in kwargs.items():
+            if not PROP2UDF.get(key):
+                raise LimsDataError(
+                    f"Unknown how to set {key} in LIMS since it is not defined in" f" {PROP2UDF}"
+                )
+            lims_sample.udf[PROP2UDF[key]] = value
+
+        lims_sample.put()
+
+    # Project ROUT
     def _export_project(lims_project) -> dict:
         """Fetch relevant information from a lims project object"""
         return {
             "id": lims_project.id,
             "name": lims_project.name,
-            "date": parse_date(lims_project.open_date) if lims_project.open_date else None,
-        }
+            "date": parse_date(lims_project.open_date) if lims_project.open_date else None, }
 
-    def _export_sample(self, lims_sample):
-        """Get data from a LIMS sample."""
-        udfs = lims_sample.udf
-        data = {
-            "id": lims_sample.id,
-            "name": lims_sample.name,
-            "project": self._export_project(lims_sample.project),
-            "family": udfs.get("familyID"),
-            "customer": udfs.get("customer"),
-            "sex": SEX_MAP.get(udfs.get("Gender"), None),
-            "father": udfs.get("fatherID"),
-            "mother": udfs.get("motherID"),
-            "source": udfs.get("Source"),
-            "status": udfs.get("Status"),
-            "panels": udfs.get("Gene List").split(";") if udfs.get("Gene List") else None,
-            "priority": udfs.get("priority"),
-            "received": self.get_received_date(lims_sample.id),
-            "application": udfs.get("Sequencing Analysis"),
-            "application_version": (
-                int(udfs["Application Tag Version"])
-                if udfs.get("Application Tag Version")
-                else None
-            ),
-            "comment": udfs.get("comment"),
-        }
+    ### Pool ROUT
+    def samples_in_pools(self, pool_name, projectname) -> List[Sample]:  ###!!!!!
+        """Fetch all samples from a pool"""
+        return self.get_samples(udf={"pool name": str(pool_name)}, projectname=projectname)
+
+    #### Family ROUT
+    def family(self, customer: str, family: str) -> Dict[str, Optional]:
+        """Fetch information about a family of samples."""
+        filters = {"customer": customer, "familyID": family}
+        lims_samples = self.get_samples(udf=filters)
+        samples_data = [self._export_sample(lims_sample) for lims_sample in lims_samples]
+        # get family level data
+        family_data = {"family": family, "customer": customer, "samples": []}
+        priorities = set()
+        panels = set()
+
+        for sample_data in samples_data:
+            priorities.add(sample_data["priority"])
+            if sample_data["panels"]:
+                panels.update(sample_data["panels"])
+            family_data["samples"].append(sample_data)
+
+        if len(priorities) == 1:
+            family_data["priority"] = priorities.pop()
+        elif "express" in priorities:
+            family_data["priority"] = "express"
+        elif "priority" in priorities:
+            family_data["priority"] = "priority"
+        elif "standard" in priorities:
+            family_data["priority"] = "standard"
+        else:
+            raise LimsDataError(f"unable to determine family priority: {priorities}")
+
+        family_data["panels"] = list(panels)
+        return family_data
+
+    ## Sample ROUT
+    def sample(self, lims_id: str) -> Dict[str, Optional[str]]:
+        """Fetch a sample from the LIMS database."""
+        lims_sample = Sample(self, id=lims_id)
+        data = self._export_sample(lims_sample)
         return data
 
-    @staticmethod
-    def _export_artifact(lims_artifact):
-        """Get data from a LIMS artifact."""
-        return {"id": lims_artifact.id, "name": lims_artifact.name}
+    def get_sample_comment(self, sample_id: str) -> str:
+        """Get the comment of the sample"""
+        lims_sample = self.sample(sample_id)
+        return lims_sample.get("comment")
+
+    def get_sample_project(self, sample_id: str) -> str:
+        """Get the lims-id for the project of the sample"""
+        return self.sample(sample_id).get("project").get("id")
+
+    def get_processing_time(self, lims_id: str) -> dt.datetime:
+        """Get the time it takes to process a sample"""
+        received_at = self.get_received_date(lims_id)
+        delivery_date = self.get_delivery_date(lims_id)
+        if received_at and delivery_date:
+            return delivery_date - received_at
+        return None
 
     def get_received_date(self, lims_id: str) -> dt.date:
         """Get the date when a sample was received."""
@@ -135,6 +228,55 @@ class LimsAPI(Lims, OrderHandler):
             date = None
         return date
 
+    def _export_sample(self, lims_sample) -> Dict[str, Optional[str]]:
+        """Get data from a LIMS sample."""
+        udfs = lims_sample.udf
+        data = {
+            "id": lims_sample.id,
+            "name": lims_sample.name,
+            "project": self._export_project(lims_sample.project),
+            "family": udfs.get("familyID"),
+            "customer": udfs.get("customer"),
+            "sex": SEX_MAP.get(udfs.get("Gender"), None),
+            "father": udfs.get("fatherID"),
+            "mother": udfs.get("motherID"),
+            "source": udfs.get("Source"),
+            "status": udfs.get("Status"),
+            "panels": udfs.get("Gene List").split(";") if udfs.get("Gene List") else None,
+            "priority": udfs.get("priority"),
+            "received": self.get_received_date(lims_sample.id),
+            "application": udfs.get("Sequencing Analysis"),
+            "application_version": (
+                int(udfs["Application Tag Version"])
+                if udfs.get("Application Tag Version")
+                else None
+            ),
+            "comment": udfs.get("comment"),
+        }
+        return data
+
+    ###All these could be saved as sample level udf
+    def get_prep_method(self, lims_id: str) -> str:
+        """Get the library preparation method."""
+
+        step_names_udfs = MASTER_STEPS_UDFS["prep_method_step"]
+
+        return self._get_methods(step_names_udfs, lims_id)
+
+    def get_sequencing_method(self, lims_id: str) -> str:
+        """Get the sequencing method."""
+
+        step_names_udfs = MASTER_STEPS_UDFS["sequencing_method_step"]
+
+        return self._get_methods(step_names_udfs, lims_id)
+
+    def get_delivery_method(self, lims_id: str) -> str:
+        """Get the delivery method."""
+
+        step_names_udfs = MASTER_STEPS_UDFS["delivery_method_step"]
+
+        return self._get_methods(step_names_udfs, lims_id)
+
     def capture_kit(self, lims_id: str) -> str:
         """Get capture kit for a LIMS sample."""
 
@@ -166,150 +308,35 @@ class LimsAPI(Lims, OrderHandler):
 
         return None
 
-    def get_samples(self, *args, map_ids=False, **kwargs):
-        """Bypass to original method."""
-        lims_samples = super(LimsAPI, self).get_samples(*args, **kwargs)
-        if map_ids:
-            lims_map = {lims_sample.name: lims_sample.id for lims_sample in lims_samples}
-            return lims_map
-
-        return lims_samples
-
-    def family(self, customer: str, family: str):
-        """Fetch information about a family of samples."""
-        filters = {"customer": customer, "familyID": family}
-        lims_samples = self.get_samples(udf=filters)
-        samples_data = [self._export_sample(lims_sample) for lims_sample in lims_samples]
-        # get family level data
-        family_data = {"family": family, "customer": customer, "samples": []}
-        priorities = set()
-        panels = set()
-
-        for sample_data in samples_data:
-            priorities.add(sample_data["priority"])
-            if sample_data["panels"]:
-                panels.update(sample_data["panels"])
-            family_data["samples"].append(sample_data)
-
-        if len(priorities) == 1:
-            family_data["priority"] = priorities.pop()
-        elif "express" in priorities:
-            family_data["priority"] = "express"
-        elif "priority" in priorities:
-            family_data["priority"] = "priority"
-        elif "standard" in priorities:
-            family_data["priority"] = "standard"
-        else:
-            raise LimsDataError(f"unable to determine family priority: {priorities}")
-
-        family_data["panels"] = list(panels)
-        return family_data
-
-    def process(self, process_id: str) -> Process:
-        """Get LIMS process."""
-        return Process(self, id=process_id)
+    ## NOT NEEDED IF SAVED ON SAMPLE????
+    @staticmethod
+    def _find_capture_kits(artifacts, udf_key: str) -> Set[str]:
+        """
+        get capture kit from parent process for non-TWIST samples
+        """
+        capture_kits = set(
+            artifact.parent_process.udf.get(udf_key)
+            for artifact in artifacts
+            if artifact.parent_process.udf.get(udf_key) is not None
+        )
+        return capture_kits
 
     @staticmethod
-    def process_samples(lims_process: Process) -> Generator[str, None, None]:
-        """Retrieve LIMS input samples from a process."""
-        for lims_artifact in lims_process.all_inputs():
-            for lims_sample in lims_artifact.samples:
-                yield lims_sample.id
-
-    def update_sample(
-        self, lims_id: str, sex=None, target_reads: int = None, name: str = None, **kwargs
-    ):
-        """Update information about a sample."""
-        lims_sample = Sample(self, id=lims_id)
-
-        if sex:
-            lims_gender = REV_SEX_MAP.get(sex)
-            if lims_gender:
-                lims_sample.udf[PROP2UDF["sex"]] = lims_gender
-        if name:
-            lims_sample.name = name
-        if isinstance(target_reads, int):
-            lims_sample.udf[PROP2UDF["target_reads"]] = target_reads
-
-        for key, value in kwargs.items():
-            if not PROP2UDF.get(key):
-                raise LimsDataError(
-                    f"Unknown how to set {key} in LIMS since it is not defined in" f" {PROP2UDF}"
-                )
-            lims_sample.udf[PROP2UDF[key]] = value
-
-        lims_sample.put()
-
-    def update_project(self, lims_id: str, name=None):
-        """Update information about a project."""
-        lims_project = Project(self, id=lims_id)
-        if name:
-            lims_project.name = name
-            lims_project.put()
-
-    def get_prep_method(self, lims_id: str) -> str:
-        """Get the library preparation method."""
-
-        step_names_udfs = MASTER_STEPS_UDFS["prep_method_step"]
-
-        return self._get_methods(step_names_udfs, lims_id)
-
-    def get_sequencing_method(self, lims_id: str) -> str:
-        """Get the sequencing method."""
-
-        step_names_udfs = MASTER_STEPS_UDFS["sequencing_method_step"]
-
-        return self._get_methods(step_names_udfs, lims_id)
-
-    def get_delivery_method(self, lims_id: str) -> str:
-        """Get the delivery method."""
-
-        step_names_udfs = MASTER_STEPS_UDFS["delivery_method_step"]
-
-        return self._get_methods(step_names_udfs, lims_id)
-
-    def get_processing_time(self, lims_id: str) -> dt.datetime:
-        """Get the time it takes to process a sample"""
-        received_at = self.get_received_date(lims_id)
-        delivery_date = self.get_delivery_date(lims_id)
-        if received_at and delivery_date:
-            return delivery_date - received_at
-        return None
-
-    @staticmethod
-    def _sort_by_date_run(sort_list: list):
+    def _find_twist_capture_kits(artifacts, udf_key) -> Set[str]:
         """
-        Sort list of tuples by parent process attribute date_run in descending order.
-
-        Parameters:
-            sort_list (list): a list of tuples in the format (date_run, elem1, elem2, ...)
-
-        Returns:
-            sorted list of tuples
+        get capture kit from parent process for TWIST samples
         """
-        return sorted(sort_list, key=lambda sort_tuple: sort_tuple[0], reverse=True)
+        capture_kits = set(
+            artifact.udf.get(udf_key)
+            for artifact in artifacts
+            if artifact.udf.get(udf_key) is not None
+        )
+        return capture_kits
 
-    def _most_recent_date(self, dates: list):
+    def _get_methods(self, step_names_udfs, lims_id) -> List[str]:
         """
-        Gets the most recent date from a list of dates sorted by date_run
-
-        Parameters:
-            dates (list): a list of tuples in the format (date_run, date), sorted by date_run
-            descending
-
-        Returns:
-            The date in the first tuple in dates
-        """
-        sorted_dates = self._sort_by_date_run(dates)
-        date_run_index = 0
-        date_index = 1
-
-        return sorted_dates[date_run_index][date_index] if dates else None
-
-    def _get_methods(self, step_names_udfs, lims_id):
-        """
-        Gets the method, method number and method version for a given list of stop names
-        """
+            Gets the method, method number and method version for a given list of stop names
+            """
         methods = []
 
         for process_name in step_names_udfs:
@@ -336,52 +363,28 @@ class LimsAPI(Lims, OrderHandler):
                     f"{method[METHOD_NUMBER_INDEX]}:{method[METHOD_VERSION_INDEX]} - "
                     f"{method_name}"
                 )
-
         return None
 
-    @staticmethod
-    def get_method_number(artifact, udf_key_number):
-        """
-        get method number for artifact
-        """
-        return artifact.parent_process.udf.get(udf_key_number)
+        @staticmethod
+        def get_method_number(artifact, udf_key_number) -> str:
+            """
+            get method number for artifact
+            """
+            return artifact.parent_process.udf.get(udf_key_number)
 
-    @staticmethod
-    def get_method_version(artifact, udf_key_version):
-        """
-        get method version for artifact
-        """
-        return artifact.parent_process.udf.get(udf_key_version)
+        @staticmethod
+        def get_method_version(artifact, udf_key_version) -> str:
+            """
+            get method version for artifact
+            """
+            return artifact.parent_process.udf.get(udf_key_version)
 
-    @staticmethod
-    def _find_capture_kits(artifacts, udf_key):
-        """
-        get capture kit from parent process for non-TWIST samples
-        """
-        capture_kits = set(
-            artifact.parent_process.udf.get(udf_key)
-            for artifact in artifacts
-            if artifact.parent_process.udf.get(udf_key) is not None
-        )
-        return capture_kits
+    ## Samples filter...
+    def get_samples(self, *args, map_ids=False, **kwargs) -> List[Sample]:  ###!!!!!!
+        """Bypass to original method."""
+        lims_samples = super(LimsAPI, self).get_samples(*args, **kwargs)
+        if map_ids:
+            lims_map = {lims_sample.name: lims_sample.id for lims_sample in lims_samples}
+            return lims_map
 
-    @staticmethod
-    def _find_twist_capture_kits(artifacts, udf_key):
-        """
-        get capture kit from parent process for TWIST samples
-        """
-        capture_kits = set(
-            artifact.udf.get(udf_key)
-            for artifact in artifacts
-            if artifact.udf.get(udf_key) is not None
-        )
-        return capture_kits
-
-    def get_sample_comment(self, sample_id: str) -> str:
-        """Get the comment of the sample"""
-        lims_sample = self.sample(sample_id)
-        return lims_sample.get("comment")
-
-    def get_sample_project(self, sample_id: str) -> str:
-        """Get the lims-id for the project of the sample"""
-        return self.sample(sample_id).get("project").get("id")
+        return lims_samples
